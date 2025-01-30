8bf31a88380821e6a715f9e73179274a39bffdcb
Prajwal <prajwaljagadeesh2001@gmail.com>
2025-01-31 01:31:21 +0530

    third commit

--- flight-status/src/App.tsx
+++ flight-status/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     <Router>
     <Routes>
       <Route path="/" element={<FlightTable />} />
-      <Route path="/flight/:id" element={<FlightDetails />} /> {/* Define route for flight details */}
+      <Route path="/flight/:id" element={<FlightDetails />} />
     </Routes>
   </Router>
   );
--- flight-status/src/routes.tsx
+++ flight-status/src/routes.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import FlightTable from "./components/FlightTable";
-import FlightDetails from "./components/FlightDetails";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import FlightTable from "./components/FlightTable";
+// import FlightDetails from "./components/FlightDetails";
 
-const AppRoutes: React.FC = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<FlightTable />} />
-      <Route path="/flights/:id" element={<FlightDetails />} />
-    </Routes>
-  );
-};
+// const AppRoutes: React.FC = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<FlightTable />} />
+//       <Route path="/flights/:id" element={<FlightDetails />} />
+//     </Routes>
+//   );
+// };
 
-export default AppRoutes;
+// export default AppRoutes;
